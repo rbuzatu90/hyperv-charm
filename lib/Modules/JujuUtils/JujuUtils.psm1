924343ae1ea4cd69094fc444849f9698a119f428
Ionut Balutoiu <ibalutoiu@cloudbasesolutions.com>
2016-02-26 17:27:10 +0200

    Changed serialization to JSON

--- lib/Modules/JujuUtils/JujuUtils.psm1
+++ lib/Modules/JujuUtils/JujuUtils.psm1
@@ -419,7 +419,7 @@ function Get-MarshaledObject {
         $Object
     )
     PROCESS {
-        $encoded = $Object | ConvertTo-Yaml
+        $encoded = $Object | ConvertTo-Json
         $b64 = ConvertTo-Base64 $encoded
         return $b64
     }
@@ -440,7 +440,7 @@ function Get-UnmarshaledObject {
     )
     PROCESS {
         $decode = ConvertFrom-Base64 $Object
-        $ret = $decode | ConvertFrom-Yaml
+        $ret = $decode | ConvertFrom-Json
         return $ret
     }
 }

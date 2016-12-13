6bceda338e24aa0da97fdbdd64fb4f1898e3b0c3
Remus Buzatu <r.buzatu90@gmail.com>
2016-12-13 10:28:26 +0200

    Removed setting an IP on vmswitch
    Signed-off-by: Remus Buzatu <r.buzatu90@gmail.com>

--- lib/Modules/HyperVCIHooks/HyperVCIHooks.psm1
+++ lib/Modules/HyperVCIHooks/HyperVCIHooks.psm1
@@ -1132,14 +1132,6 @@ function Start-ConfigureVMSwitch {
     Write-JujuInfo "Adding new vmswitch: $VMswitchName"
     New-VMSwitch -Name $VMswitchName -NetAdapterName $dataPort.Name -AllowManagementOS $managementOS
 
-    $vmswitch_index=(Get-NetAdapter | ? Name -Like "*$VMswitchName*").ifIndex
-    $primary_interface=(Get-NetIPConfiguration | Foreach IPv4DefaultGateway).ifIndex
-    $2nd_octet=(Get-NetIPAddress -ifIndex $primary_interface | ? AddressFamily -eq IPv4).IPAddress.split(".")[1]
-    $4th_octet=(Get-NetIPAddress -ifIndex $primary_interface | ? AddressFamily -eq IPv4).IPAddress.split(".")[3]
-    $new_ip="10.250.$2nd_octet.$4th_octet"
-    Write-JujuInfo "Setting IP addres of $new_ip to br100"
-    New-NetIPAddress -ifIndex $vmswitch_index -IPAddress $new_ip -PrefixLength 16 -ErrorAction SilentlyContinue
-
     return $true
 }
 
